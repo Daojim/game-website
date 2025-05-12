394439f6d62e53227cad10da9f70dab668705b18
Daojim <jimmyddao@gmail.com>
2025-05-12 16:52:32 -0400

    Add page routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home";
+import Favourites from "./pages/Favourites";
 
 function App() {
   return (
-    <>
-      <Home />
-    </>
+    <main className="main-content">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/favourites" element={<Favourites />} />
+      </Routes>
+    </main>
   );
 }
 
--- src/pages/Favourites.jsx
+++ src/pages/Favourites.jsx
@@ -1,4 +1,4 @@
-function Favourite() {
+function Favourites() {
   return (
     <div className="favourites-empty">
       <h2>No Favourite Games Yet</h2>
@@ -7,4 +7,4 @@ function Favourite() {
   );
 }
 
-export default Favourite;
+export default Favourites;
