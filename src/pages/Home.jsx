2a6844edff82fe5268a08c75fc6ba508c5a47bda
Daojim <jimmyddao@gmail.com>
2025-05-12 16:34:12 -0400

    Add search handling with state

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,13 +2,19 @@ import GameCard from "../components/GameCard";
 import { useState } from "react";
 
 function Home() {
+  const [searchQuery, setSearchQuery] = useState("");
+
   const games = [
     { id: 1, title: "Hollow Knight", release_date: "2017" },
     { id: 2, title: "Elden Ring", release_date: "2022" },
     { id: 3, title: "Clair Obscur: Expedition 33", release_date: "2025" },
   ];
 
-  const handleSearch = () => {};
+  const handleSearch = (e) => {
+    e.preventDefault();
+    alert(searchQuery);
+    setSearchQuery("");
+  };
 
   return (
     <div className="home">
@@ -17,6 +23,8 @@ function Home() {
           type="text"
           placeholder="Search for games..."
           className="search-input"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
         />
         <button type="submit" className="search-btn">
           Search
