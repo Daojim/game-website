332c7b54f003e16e740304d4acb97c38ca093c21
Daojim <jimmyddao@gmail.com>
2025-05-12 16:15:09 -0400

    Add Favourites.jsx and test GameCard component in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import "./App.css";
+import GameCard from "./components/GameCard";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <GameCard game={{ title: "Jimmy's Game", release_date: "2025" }} />
+      <GameCard game={{ title: "AAA Game", release_date: "2025" }} />
+    </>
+  );
 }
 
 export default App;
